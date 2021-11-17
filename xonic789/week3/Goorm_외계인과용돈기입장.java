d6222bf1a122f190ee5426f61deb28fbf8badbf5
xonic789 <xonic789@naver.com>
2021-11-18 00:57:17 +0900

    xonic789 !: Goorm 외계인과 용돈 기입장
    각 구간까지의 합을 배열에 저장해놓고,
    구간이 나오면 구간 합의 배열에
    끝 - 시작점의 전 구간 합을 하면 답이 나온다.

--- xonic789/week3/Goorm_외계인과용돈기입장.java
+++ xonic789/week3/Goorm_외계인과용돈기입장.java
@@ -9,22 +9,27 @@ public class Goorm_외계인과용돈기입장 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String[] input = br.readLine().split(" ");
-    int M = Integer.parseInt(input[1]);
+    int N = Integer.parseInt(input[0]), M = Integer.parseInt(input[1]);
     String[] history = br.readLine().split(" ");
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    long[] sums = new long[N + 1];
+    int sum = 0;
+    for (int i = 1; i <= N; i++) {
+      char sign = history[i - 1].charAt(0);
+      if (sign == '+') {
+        sum += Integer.parseInt(history[i - 1].substring(1));
+      } else {
+        sum -= Integer.parseInt(history[i - 1].substring(1));
+      }
+      sums[i] = sum;
+    }
+
     for (int i = 0; i < M; i++) {
       String[] s = br.readLine().split(" ");
-      int start = Integer.parseInt(s[0]) - 1, end = Integer.parseInt(s[1]);
-      int sum = 0;
-      for (int j = start; j < end; j++) {
-        char sign = history[j].charAt(0);
-        if (sign == '+') {
-          sum += Integer.parseInt(history[j].substring(1));
-        } else {
-          sum -= Integer.parseInt(history[j].substring(1));
-        }
-      }
-      bw.write(sum > 0 ? "+" + sum : String.valueOf(sum));
+      int start = Integer.parseInt(s[0]), end = Integer.parseInt(s[1]);
+      long answer = sums[end] - sums[start - 1];
+      bw.write(answer > 0 ? "+" + answer : String.valueOf(answer));
+      bw.write("\n");
     }
     bw.flush();
   }
