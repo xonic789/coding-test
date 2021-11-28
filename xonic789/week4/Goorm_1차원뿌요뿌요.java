0f09f8b88c17a7eb0007ae0fc07eace1175a4ff4
xonic789 <xonic789@naver.com>
2021-11-28 21:33:53 +0900

    xonic789 !: Goorm 1차원 뿌요뿌요
    코드 리팩토링,.

--- xonic789/week4/Goorm_1차원뿌요뿌요.java
+++ xonic789/week4/Goorm_1차원뿌요뿌요.java
@@ -11,14 +11,15 @@ public class Goorm_1차원뿌요뿌요 {
     String[] input = br.readLine().split(" ");
     int N = Integer.parseInt(input[0]);
     int M = Integer.parseInt(input[1]);
-    String str = br.readLine();
+    String str = br.readLine() + "0";
     // 터져야하는 위치를 담음.
     Stack<String> stack = new Stack<>();
 
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N + 1; i++) {
+      // 비어있으면 무조건 넣음.
       if (stack.isEmpty()) {
-        stack.push(str.charAt(0) + " " + 1);
+        stack.push(str.charAt(i) + " " + 1);
         continue;
       }
       if (stack.peek().charAt(0) == str.charAt(i)) {
@@ -28,39 +29,29 @@ public class Goorm_1차원뿌요뿌요 {
         stack.pop();
         stack.push(alpha + " " + (cnt + 1));
       } else {
-        while (!stack.isEmpty()) {
-          String[] peek = stack.peek().split(" ");
-          int cnt = Integer.parseInt(peek[1]);
-          if (cnt >= M) stack.pop();
-          else break;
-        }
-        if (!stack.isEmpty() && stack.peek().charAt(0) == str.charAt(i)) {
-          String[] peek = stack.peek().split(" ");
-          char alpha = peek[0].charAt(0);
-          int cnt = Integer.parseInt(peek[1]);
+        String[] peek = stack.peek().split(" ");
+        char alpha = peek[0].charAt(0);
+        int cnt = Integer.parseInt(peek[1]);
+        if (cnt >= M) {
           stack.pop();
-          stack.push(alpha + " " + (cnt + 1));
-        } else stack.push(str.charAt(i) + " " + 1);
+          i--;
+        } else {
+          stack.push(str.charAt(i) + " " + 1);
+        }
       }
 
     }
-    if (stack.size() == 1){
-      String[] s = stack.peek().split(" ");
-      int cnt = Integer.parseInt(s[1]);
-      if (cnt >= M) System.out.println("CLEAR!");
-      else {
-        System.out.print(String.valueOf(s[0]).repeat(Math.max(0, cnt)));
-      }
-    } else {
-      for (String s : stack) {
-        String[] s1 = s.split(" ");
-        char alpha = s.charAt(0);
-        int cnt = Integer.parseInt(s1[1]);
-        if (cnt >= M) System.out.print(String.valueOf(s1[0]).repeat(Math.max(0, cnt)));
-        else System.out.print(alpha);
-      }
+    //
+    stack.pop();
+
+    if (stack.isEmpty()) {
+      System.out.println("CLEAR!");
+      return;
+    }
+
+    for (String s : stack) {
+      String[] s1 = s.split(" ");
+      System.out.print(String.valueOf(s1[0]).repeat(Math.max(0, Integer.parseInt(s1[1]))));
     }
   }
 }
-// ABEBADBDCDABABEAEDACABDC
-// ABEBADBDCDABABEAEDACABDC
