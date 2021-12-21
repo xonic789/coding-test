0b407465df667a4ee66582250b571e8a6309557c
hwangjeunghwan <j_h6467@naver.com>
2021-12-21 18:08:55 +0900

    BOJ 이항계수3

--- hwangjeunghwan/BOJ/src/가운데를말해요.java
+++ hwangjeunghwan/BOJ/src/가운데를말해요.java
@@ -18,10 +18,10 @@ public class 가운데를말해요 {
 	        for(int i = 0; i < n; i++) {
 	            int num = sc.nextInt();
 	            
-	            if(minQ.size() == maxQ.size()) maxQ.add(num);
-	            else minQ.add(num);
+	            if(minQ.size() == maxQ.size()) maxQ.add(num); //사이즈가 같으면 최대힙 넣기
+	            else minQ.add(num); //다르면 최소힙 넣기
 	            
-	            if(!minQ.isEmpty() && !maxQ.isEmpty() && minQ.peek() < maxQ.peek()) {
+	            if(!minQ.isEmpty() && !maxQ.isEmpty() && minQ.peek() < maxQ.peek()) { //최소의 현재값보다 최대의 현재값 크면 스왑
 	                int temp = minQ.poll();
 	                minQ.add(maxQ.poll());
 	                maxQ.add(temp);
