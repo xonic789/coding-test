bfd8aea60cd066ee405d73fa7475485d507fd39a
hwangjeunghwan <j_h6467@naver.com>
2021-12-21 18:09:54 +0900

    Goorm 다람쥐와 도토리

--- hwangjeunghwan/BITA/src/Chapter2/Chapter2_8.java
+++ hwangjeunghwan/BITA/src/Chapter2/Chapter2_8.java
@@ -1,37 +1,44 @@
 package Chapter2;
-
 import java.util.Scanner;
-
-//해설지를 이해하지 못 하겠습니다.
 public class Chapter2_8 {
 	
-final static int MOD = 1000000007;	
+final static long P = 1000000007;	
 
-static long C(int n, int r) {
-	long a = 1, b = 1;
+static long C(long n, long r) {
+	long a = 1L, b = 1L;
 
     // a = n! / (n-r)!
     // b = r!
     for (int i = 0; i < r; ++i) {
-        a = a * (n - i) % MOD;
-        b = b * (i + 1) % MOD;
+        a = a * (n - i) % P;
+        b = b * (i + 1) % P;
     }
 
     // nCr = a / b = a * b^-1
-    return a * fpow(b, MOD - 2) % MOD;
+    return a * pow(b, P - 2) % P;
 }
 
-static long fpow(long b, int n) {
-	long ret = 1;
-	// n을 이진수로 생각하여 해당 비트에 대응되는 a^(2^k)을 곱한다
-    while (n>0) {
-        if ((n&1)==0) {
-        	ret = ret * b % MOD;
-        	}
-        b =  b * b % MOD;
-        n >>= 1;
-    }
-    return ret;
+public static long pow(long base, long expo) {
+	
+	// 지수가 1일 경우 base^1 이므로 base % P를 리턴
+	if(expo == 1) {
+		return base % P;
+	}
+	// 지수의 절반에 해당하는 base^(expo / 2) 을 구한다.
+	long temp = pow(base, expo / 2);
+	
+	/*
+	 * 현재 지수가 홀 수 였다면
+	 * base^(expo / 2) * base^(expo / 2) * base 이므로
+	 * base를 한 번 더 곱해주어야 한다.
+	 * 
+	 * ex) A^9 = A^4 * A^4 * A
+	 */
+	if(expo % 2 == 1) {
+		return (temp * temp % P) * base % P;
+	}
+	return temp * temp % P;
+			
 }
 	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
@@ -50,19 +57,19 @@ static long fpow(long b, int n) {
 
 	    // 시작위치 -> 도토리에 함정이 있는 경우
 	    if (x2 <= x1 && y2 <= y1)
-	        a -= C(x2 + y2, x2) * C(x1 - x2 + y1 - y2, x1 - x2) % MOD;
+	        a -= C(x2 + y2, x2) * C(x1 - x2 + y1 - y2, x1 - x2) % P;
 
 	    // 도토리 -> 최종위치에 함정이 있는 경우
 	    if (x1 <= x2 && y1 <= y2)
-	        b -= C(x2 - x1 + y2 - y1, x2 - x1) * C(N - x2 + M - y2, N - x2) % MOD;
+	        b -= C(x2 - x1 + y2 - y1, x2 - x1) * C(N - x2 + M - y2, N - x2) % P;
 
 	    // 음수가 됐다면 양수로 만들어 준다
-	    if (a < 0) a += MOD;
-	    if (b < 0) b += MOD;
+	    if (a < 0) a += P;
+	    if (b < 0) b += P;
 
 	    
 
-	    System.out.println(a*b%MOD);
+	    System.out.println(a*b%P);
 		
 		
 	}
